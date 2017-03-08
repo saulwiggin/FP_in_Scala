db26500de720bda79972df33c2c135ae44af67e1
saul wiggin <saulwiggin@googlemail.com>
2017-03-08 13:57:30 +0000

    Initial commit

---
+++ src/simpleprogram.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by user on 08/03/17.
+  */
+class Cafe {
+  def buyCoffee(cc: CreditCard): (Coffee, Charge) = {
+    val cup = new Coffee()
+    (cup, Charge(cc, cup.price))
+  }
+}
